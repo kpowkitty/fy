4fd2595a3d663d842f1fc5c87d795eb4926f6842
kpowkitty <witchikittikat@gmail.com>
2024-03-05 02:44:34 -0800

    final

--- src/main.cpp
+++ src/main.cpp
@@ -19,6 +19,11 @@
 void display_main_menu();
 
 /**
+ * @brief Displays the submenu for financial-aid related questions.
+ */
+void display_financial_loss_submenu();
+
+/**
  * @brief Displays the submenu for credit-related questions.
  */
 void display_credit_submenu();
@@ -41,7 +46,7 @@ void display_feminine_submenu();
 int main() {
     AI ai;
     char choice;
-    std::string processing = "Let me think for a moment...\n\n";
+    std::string processing = "\nLet me think for a moment...\n\n";
 
     std::cout << "Hi, I'm Fy, your financial helper! "
               << "Please choose a prompt." << std::endl;
@@ -86,13 +91,14 @@ int main() {
                             case '7':
                                 break;
                             default:
-                                std::cout << "Invalid choice. "
+                                std::cout << "\nInvalid choice. "
                                           << "Please select a number "
                                           << "from 1-7." << std::endl;
+                                break;
                         }
                     }
                     catch (const std::exception& e) {
-                        std::cerr << "Invalid choice. "
+                        std::cerr << "\nInvalid choice. "
                                   << "Please enter a valid number."
                                   << std::endl;
             			std::cin.clear();
@@ -122,13 +128,14 @@ int main() {
                             case '4':
                                 break;
                             default:
-                                std::cout << "Invalid choice. "
+                                std::cout << "\nInvalid choice. "
                                           << "Please select a number "
                                           << "from 1-4." << std::endl;
+                                break;
                         }
                     }
                     catch (const std::exception& e) {
-                        std::cerr << "Invalid input. "
+                        std::cerr << "\nInvalid input. "
                                   << "Please enter a valid number."
                                   << std::endl;
                         std::cin.clear();
@@ -162,13 +169,14 @@ int main() {
                             case '5':
                                 break;
                             default:
-                                std::cout << "Invalid choice. "
+                                std::cout << "\nInvalid choice. "
                                           << "Please select a number "
                                           << "from 1-5." << std::endl;
+                                break;
                         }
                     }
                     catch (const std::exception& e) {
-                        std::cerr << "Invalid input. "
+                        std::cerr << "\nInvalid input. "
                                   << "Please enter a valid number."
                                   << std::endl;
                         std::cin.clear();
@@ -210,13 +218,14 @@ int main() {
                             case '7':
                                 break;
                             default:
-                                std::cout << "Invalid choice. "
+                                std::cout << "\nInvalid choice. "
                                           << "Please select a number "
                                           << "from 1-5." << std::endl;
+                                break;
                         }
                     }
                     catch (const std::exception& e) {
-                        std::cerr << "Invalid input. "
+                        std::cerr << "\nInvalid input. "
                                   << "Please enter a valid number."
                                   << std::endl;
                         std::cin.clear();
@@ -224,37 +233,109 @@ int main() {
                 } while (feminine_choice != '7');
                 break;
             case '5':
-                std::cout << "Goodbye! It was a pleasure assisting you. "
+                char fin_aid_choice;
+                do {
+                    display_financial_loss_submenu();
+                    fin_aid_choice = getchar();
+
+                    try {
+                        switch (fin_aid_choice) {
+                            case '1':
+                                std::cout << processing;
+                                ai.question("What financial options are there for someone who is homeless?");
+                                break;
+                            case '2':
+                                std::cout << processing;
+                                ai.question("What are homeless shelters?");
+                                break;
+                            case '3':
+                                std::cout << processing;
+                                ai.question("How can I get into a homeless shelter?");
+                                break;
+                            case '4':
+                                std::cout << processing;
+                                ai.question("What is bankruptcy?");
+                                break;
+                            case '5':
+                                std::cout << processing;
+                                ai.question("When would filing bankruptcy be helpful?");
+                                break;
+                            case '6':
+                                std::cout << processing;
+                                ai.question("How can I file for bankruptcy?");
+                                break;
+                            case '7':
+                                std::cout << processing;
+                                ai.question("What can I do if I have enormous debt?");
+                                break;
+                            case '8':
+                                break;
+                            default:
+                                std::cout << "\nInvalid choice. "
+                                          << "Please select a number "
+                                          << "from 1-5." << std::endl;
+                                break;
+                        }
+                    }
+                    catch (const std::exception& e) {
+                        std::cerr << "\nInvalid input. "
+                                  << "Please enter a valid number."
+                                  << std::endl;
+                        std::cin.clear();
+                    }
+                } while (fin_aid_choice != '8');
+                break;
+            /*case '6': {
+                std::string user_question;
+                bool exit = false;
+                do {
+                    std::cin.clear();
+                    std::cout << "What would you like to ask me?\n";
+                    if (std::getline(std::cin, user_question)) {
+                        ai.question(user_question);
+                    }
+                    std::cout << "Type exit to go back, or ask me another question!";
+                    if (user_question == "exit") {
+                        exit == true;
+                    }
+                } while (!exit);
+            }
+                break;*/
+            case '6':
+                std::cout << "\nGoodbye! It was a pleasure assisting you. "
                           << "Have a wonderful day!" << std::endl;
                 break;
             default:
-                std::cout << "Invalid choice. "
+                std::cout << "\nInvalid choice. "
                           << "Please select a number from 1-5."
                           << std::endl;
+                break;
             }
         }
         catch (const std::exception& e) {
-            std::cerr << "Invalid input. Please enter a valid number."
+            std::cerr << "\nInvalid input. Please enter a valid number."
                       << std::endl;
             std::cin.clear();
         }
-    } while (choice != '5');
+    } while (choice != '6');
 
     return 0;
 }
 
 void display_main_menu() {
-    std::cout << "-----Main Menu-----\n"
-              //<< "1. Ask Fy anything\n"
+    std::cout << "\n-----Main Menu-----\n"
               << "1. Credit Questions\n"
               << "2. Government Aid Questions\n"
               << "3. College Questions\n"
               << "4. Feminine Questions\n"
-              << "5. Exit" << std::endl;
+              << "5. Financial Loss Questions\n"
+              //<< "6. Ask Fy anything\n"
+              << "6. Exit" << std::endl;
 }
 
+
 void display_credit_submenu() {
-    std::cout << "-----Credit Submenu-----\n"
+    std::cout << "\n-----Credit Submenu-----\n"
               << "1. What is Credit?\n"
               << "2. Best Starter Credit Cards\n"
               << "3. What are credit rewards?\n"
@@ -265,7 +346,7 @@ void display_credit_submenu() {
 }
 
 void display_government_submenu() {
-    std::cout << "-----Government Aid Submenu-----\n"
+    std::cout << "\n-----Government Aid Submenu-----\n"
               << "1. What California government aid is out there?\n"
               << "2. What is SNAP/TANF?\n"
               << "3. Am I eligible for SNAP/TANF?\n"
@@ -273,7 +354,7 @@ void display_government_submenu() {
 }
 
 void display_college_submenu() {
-    std::cout << "-----College Submenu-----\n"
+    std::cout << "\n-----College Submenu-----\n"
               << "1. What are scholarships?\n"
               << "2. Where can I attend college for free?\n"
               << "3. What is FAFSA?\n"
@@ -282,7 +363,7 @@ void display_college_submenu() {
 }
 
 void display_feminine_submenu() {
-    std::cout << "-----Feminine Submenu-----\n"
+    std::cout << "\n-----Feminine Submenu-----\n"
               << "1. What is the average pay difference between men and women?\n"
               << "2. What can women do to fight financial insecurities?\n"
               << "3. What financial options are there for single mothers?\n"
@@ -291,3 +372,17 @@ void display_feminine_submenu() {
               << "6. How can I budget around multiple children?\n"
               << "7. Go back to main menu" << std::endl;
 }
+
+void display_financial_loss_submenu() {
+    std::cout << "\n-----Financial Loss Submenu-----\n"
+              << "1. What financial options are there for "
+              << "someone who is homeless?\n"
+              << "2. What are homeless shelters?\n"
+              << "3. How can I get into a homeless shelter?\n"
+              << "4. What is bankruptcy?\n"
+              << "5. When would filing bankruptcy be helpful?\n"
+              << "6. How can I file for bankruptcy?\n"
+              << "7. What can I do if I have enourmous debt?\n"
+              << "8. Exit" << std::endl;
+}
+
