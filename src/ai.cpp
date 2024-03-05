2c146c4e0f042d952292d0b79a0bec02af5440bb
kpowkitty <witchikittikat@gmail.com>
2024-03-04 21:58:42 -0800

    finalizing things

--- src/ai.cpp
+++ src/ai.cpp
@@ -8,6 +8,9 @@
 
 #include <iostream>
 #include <string>
+#include <vector>
+#include <termios.h>
+#include <unistd.h>
 
 /** AI.
 * @param none
@@ -25,36 +28,17 @@ AI::AI() : _ai(), _conversation()
     }
 }
 
-/** Response.
+/** Get question.
 * @param none
 * @return none
-* @remarks Function for talking to the AI.
-*           Asks user for input, awaits input. Generates conversation response.
-*           Prints response to console.
-*           Loops if user wants to ask more.
+* @remarks Grabs the question the user wants to ask.
 */
 
-void AI::response() {
-    // holds next user input
-    std::string input = "";
-    // get next user input
+/*void AI::get_question(std::string& my_string) {
     std::cout << "What would you like to ask me?\n";
-    std::getline(std::cin, input);
+    std::getline(std::cin, my_string);
     std::cin.clear();
-
-    // add user input to conversation
-    _conversation.AddUserData(input);
-
-    // get response from OpenAI
-    liboai::Response response = _ai.ChatCompletion->create(
-        "gpt-3.5-turbo", _conversation);
-
-    // update our conversation with the responseconvo
-    _conversation.Update(response);
-
-    // print the response
-    std::cout << "Fy: " << _conversation.GetLastResponse() << std::endl;
-}
+}*/
 
 /** Question.
 * @param string for question to be asked
@@ -72,7 +56,120 @@ void AI::question(const std::string& question) {
     _conversation.Update(response);
 
     // print the response
-    std::cout << "Fy: " << _conversation.GetLastResponse() << std::endl;
+    //std::cout << "Fy: " << _conversation.GetLastResponse() << std::endl;
+    std::string conv = _conversation.GetLastResponse();
+    std::cout << word_wrapper(conv) << "\n\n";
+    std::cout << "What other questions do you have?" << std::endl;
 }
 
-int main() { return 0; }
+/** Word wrapper.
+* @param string to be wrapped
+* @return none
+* @remarks Takes the AI's string and wraps it to 80 cols.
+*/
+std::string AI::word_wrapper(std::string& my_string) {
+    std::stringstream new_string;
+    new_string << my_string;
+    // vector to create formatted_output easier(?) with .push_back()
+    std::vector<std::string> formatted_string;
+    // current piece of my_string that we are looking at
+    std::string current;
+    std::string white_space = " ";
+    int i = 0;
+    while (new_string >> current) {
+        // adds the amount of characters + 1 for white space
+        i += current.length() + 1;
+        // add current word to vector
+        formatted_string.push_back(current);
+        // add a space to vector because our current string isnt grabbing it
+        formatted_string.push_back(white_space);
+        // if we are at 80 chars, then insert newline (estimated)
+        if (i >= 80) {
+            formatted_string.push_back("\n");
+            i = 0;
+        }
+        // problem: gpt likes numbered lists...
+        if (is_list(formatted_string, current, i)) {
+            numbered_list(formatted_string, current);
+            i = 0;
+        }
+    }
+    std::string final_string;
+    for (auto& e : formatted_string) {
+        final_string += e;
+    }
+    return final_string;
+}
+
+
+/** Is list.
+* @param current word, vector to check, and the current count of characters
+* @return bool
+* @remarks check if current string's first character is == a number
+*         AND the next character is a "." (OR the next char is another digit
+*         followed by a ".")
+*         i.e. current
+*/
+bool AI::is_list(std::vector<std::string>& my_vector, std::string& current,
+                    int i) {
+    // initialize all chars so they are null if they do not meet conditions
+    char first_char = current[0];
+    char second_char;
+    char third_char;
+    // if the current string's length is == 2 or 3, then it is possibly a
+    // numbered list!...so let's check:
+    if (current.length() == 2) {
+        // for single digit numbered lists "2."
+        second_char = current[current.length() - 1];
+        two_digit = true;
+    } else if (current.length() == 3) {
+        // for double digit numbered lists "10."
+        second_char = current[current.length() - 2];
+        third_char = current[current.length() - 1];
+        three_digit = true;
+    }
+    // if we haven't recently done a newline, and the form is either "n."
+    // or "nn.", then return true, where n are numbers
+    if ((i != 0 && !isalpha(first_char))
+            && (second_char == '.' ||
+                (!isalpha(second_char) && third_char == '.'))) {
+            return true;
+    }
+    // if we didn't return in the if statement above, then we will reset our
+    // bools to false to guard against false flagging
+    two_digit = false;
+    three_digit = false;
+    return false;
+}
+
+/** Numbered list.
+* @param current word and vector to check
+* @return void, modifies the string obj in parameter
+* @remarks this is only entered if the function passes the "is_list" check.
+*           if it does, then it inserts a newline before the numbered point.
+*/
+
+void AI::numbered_list(std::vector<std::string>& my_vector, std::string& current) {
+    auto iterCurrent = my_vector.begin();
+    auto iterEnd = my_vector.end();
+    int size = static_cast<int>(my_vector.size());
+    int count = 0;
+    // this traverses the vector to the point right before the numbered point
+    while (count < (size - 2) && iterCurrent != iterEnd) {
+        ++count;
+        ++iterCurrent;
+    }
+    my_vector.insert(iterCurrent, "\n");
+}
+
+void AI::hide_keystrokes() {
+    termios tty;
+
+    tcgetattr(STDIN_FILENO, &tty);
+
+    /* we want to disable echo */
+    tty.c_lflag &= (~ICANON & ~ECHO);
+
+    tcsetattr(STDIN_FILENO, TCSANOW, &tty);
+}
+
--- src/main.cpp
+++ src/main.cpp
@@ -4,11 +4,14 @@
 * @developers @huynhkatrina, @kpowkitty
 */
 
-//#include "ai.h"
+#include "ai.h"
+#include "liboai.h"
 
 #include <stdlib.h>
 #include <string>
+#include <termios.h>
 #include <iostream>
+#include <unistd.h>
 
 /**
  * @brief Displays the main menu options.
@@ -30,155 +33,204 @@ void display_government_submenu();
  */
 void display_college_submenu();
 
+/**
+ * @brief Displays the submenu for feminine-related questions.
+ */
+void display_feminine_submenu();
+
 int main() {
-    //AI ai;
-    std::string answer;
-    int choice;
+    AI ai;
+    char choice;
+    std::string processing = "Let me think for a moment...\n\n";
 
     std::cout << "Hi, I'm Fy, your financial helper! "
-              << "Please choose a prompt to get started." << std::endl;
+              << "Please choose a prompt." << std::endl;
+    ai.hide_keystrokes();
 
     do {
         display_main_menu();
-        std::cin >> answer;
-
+        choice = getchar();
         try {
-            choice = std::stoi(answer);
-
             switch (choice) {
-                case 1:
+            case '1':
+                char credit_choice;
+                do {
+                    display_credit_submenu();
+                    credit_choice = getchar();
                     try {
-                        //ai.response();
+                        switch (credit_choice) {
+                            case '1':
+                                std::cout << processing;
+                                ai.question("What is Credit?");
+                                break;
+                            case '2':
+                                std::cout << processing;
+                                ai.question("Best Starter Credit Cards");
+                                break;
+                            case '3':
+                                std::cout << processing;
+                                ai.question("What are credit rewards?");
+                                break;
+                            case '4':
+                                std::cout << processing;
+                                ai.question("How can I avoid debt?");
+                                break;
+                            case '5':
+                                std::cout << processing;
+                                ai.question("How can I build better credit?");
+                                break;
+                            case '6':
+                                std::cout << processing;
+                                ai.question("How often should I open credit lines?");
+                                break;
+                            case '7':
+                                break;
+                            default:
+                                std::cout << "Invalid choice. "
+                                          << "Please select a number "
+                                          << "from 1-7." << std::endl;
+                        }
                     }
-                    catch (std::exception& e) {
-                        // this means the conversation broke
-                        std::cout << e.what() << std::endl;
+                    catch (const std::exception& e) {
+                        std::cerr << "Invalid choice. "
+                                  << "Please enter a valid number."
+                                  << std::endl;
+            			std::cin.clear();
+        			}
+                } while (credit_choice != '7');
+                break;
+            case '2':
+                char government_choice;
+                do {
+                    display_government_submenu();
+                    government_choice = getchar();
+
+                    try {
+                        switch (government_choice) {
+                            case '1':
+                                std::cout << processing;
+                                ai.question("What California government aid is out there?");
+                                break;
+                            case '2':
+                                std::cout << processing;
+                                ai.question("What is SNAP/TANF?");
+                                break;
+                            case '3':
+                                std::cout << processing;
+                                ai.question("Am I eligible for SNAP/TANF?");
+                                break;
+                            case '4':
+                                break;
+                            default:
+                                std::cout << "Invalid choice. "
+                                          << "Please select a number "
+                                          << "from 1-4." << std::endl;
+                        }
                     }
-                    break;
-                case 2:
-                    int credit_choice;
-                    do {
-                        display_credit_submenu();
-                        std::cin >> answer;
+                    catch (const std::exception& e) {
+                        std::cerr << "Invalid input. "
+                                  << "Please enter a valid number."
+                                  << std::endl;
+                        std::cin.clear();
+                    }
+                } while (government_choice != '4');
+                break;
+            case '3':
+                char college_choice;
+                do {
+                    display_college_submenu();
+                    college_choice = getchar();
 
-                        try {
-                            credit_choice = std::stoi(answer);
-                            switch (credit_choice) {
-                                case 1:
-                                    //ai.question("What is Credit?"");
-                                    break;
-                                case 2:
-                                    //ai.question("Best Starter Credit Cards");
-                                    break;
-                                case 3:
-                                    //ai.question("What are credit rewards?");
-                                    break;
-                                case 4:
-                                    //ai.question("How can I avoid debt?");
-                                    break;
-                                case 5:
-                                    //ai.question("How can I build better credit?");
-                                    break;
-                                case 6:
-                                    //ai.question("How often should I open credit lines?");
-                                    break;
-                                case 7:
-                                    break;
-                                default:
-                                    std::cout << "Invalid choice. "
-                                              << "Please select a number "
-                                              << "from 1-7." << std::endl;
-                            }
+                    try {
+                        switch (college_choice) {
+                            case '1':
+                                std::cout << processing;
+                                ai.question("What are scholarships?");
+                                break;
+                            case '2':
+                                std::cout << processing;
+                                ai.question("Where can I attend college for free?");
+                                break;
+                            case '3':
+                                std::cout << processing;
+                                ai.question("What is FAFSA?");
+                                break;
+                            case '4':
+                                std::cout << processing;
+                                ai.question("How do I know I am eligible for FAFSA");
+                                break;
+                            case '5':
+                                break;
+                            default:
+                                std::cout << "Invalid choice. "
+                                          << "Please select a number "
+                                          << "from 1-5." << std::endl;
                         }
-                        catch (const std::exception& e) {
-                            std::cerr << "Invalid choice. "
-                                      << "Please enter a valid number."
-                                      << std::endl;
-            				std::cin.clear();
-        				}
-                    } while (credit_choice != 7);
-                    break;
-                case 3:
-                    int government_choice;
-                    do {
-                        display_government_submenu();
-                        std::cin >> answer;
+                    }
+                    catch (const std::exception& e) {
+                        std::cerr << "Invalid input. "
+                                  << "Please enter a valid number."
+                                  << std::endl;
+                        std::cin.clear();
+                    }
+                } while (college_choice != '5');
+                break;
+            case '4':
+                char feminine_choice;
+                do {
+                    display_feminine_submenu();
+                    feminine_choice = getchar();
 
-                        try {
-                            government_choice = std::stoi(answer);
-                            switch (government_choice) {
-                                case 1:
-                                    //ai.question("What California government aid is out there?");
-                                    break;
-                                case 2:
-                                    //ai.question("What is SNAP/TANF?");
-                                    break;
-                                case 3:
-                                    //ai.question("Am I eligible for SNAP/TANF?");
-                                    break;
-                                case 4:
-                                    //ai.question("Go back to main menu");
-                                    break;
-                                default:
-                                    std::cout << "Invalid choice. "
-                                              << "Please select a number "
-                                              << "from 1-4." << std::endl;
-                            }
+                    try {
+                        switch (feminine_choice) {
+                            case '1':
+                                std::cout << processing;
+                                ai.question("What is the average pay difference between men and women?");
+                                break;
+                            case '2':
+                                std::cout << processing;
+                                ai.question("What can women do to fight financial insecurities?");
+                                break;
+                            case '3':
+                                std::cout << processing;
+                                ai.question("What financial options are there for single mothers?");
+                                break;
+                            case '4':
+                                std::cout << processing;
+                                ai.question("What is child support?");
+                                break;
+                            case '5':
+                                std::cout << processing;
+                                ai.question("How can I budget around a child?");
+                                break;
+                            case '6':
+                                std::cout << processing;
+                                ai.question("How can I budget around multiple children?");
+                                break;
+                            case '7':
+                                break;
+                            default:
+                                std::cout << "Invalid choice. "
+                                          << "Please select a number "
+                                          << "from 1-5." << std::endl;
                         }
-                        catch (const std::exception& e) {
-                            std::cerr << "Invalid input. "
-                                      << "Please enter a valid number."
-                                      << std::endl;
-                            std::cin.clear();
-                        }
-                    } while (government_choice != 4);
-                    break;
-                case 4:
-                    int college_choice;
-                    do {
-                        display_college_submenu();
-                        std::cin >> answer;
-
-                        try {
-                            college_choice = std::stoi(answer);
-                            switch (college_choice) {
-                                case 1:
-                                    //ai.question("What are scholarships?");
-                                    break;
-                                case 2:
-                                    //ai.question("Where can I attend college for free?");
-                                    break;
-                                case 3:
-                                    //ai.question("What is FAFSA?");
-                                    break;
-                                case 4:
-                                    //ai.question("How do I know I am eligible for FAFSA");
-                                    break;
-                                case 5:
-                                    //ai.question("Go back to main menu");
-                                    break;
-                                default:
-                                    std::cout << "Invalid choice. "
-                                              << "Please select a number "
-                                              << "from 1-5." << std::endl;
-                            }
-                        }
-                        catch (const std::exception& e) {
-                            std::cerr << "Invalid input. "
-                                      << "Please enter a valid number."
-                                      << std::endl;
-                            std::cin.clear();
-                        }
-                    } while (college_choice != 5);
-                    break;
-                case 5:
-                    std::cout << "Goodbye! It was a pleasure assisting you. "
-                              << "Have a wonderful day!" << std::endl;
-                    break;
-                default:
-                    std::cout << "Invalid choice. "
-                              << "Please select a number from 1-5."
-                              << std::endl;
+                    }
+                    catch (const std::exception& e) {
+                        std::cerr << "Invalid input. "
+                                  << "Please enter a valid number."
+                                  << std::endl;
+                        std::cin.clear();
+                    }
+                } while (feminine_choice != '7');
+                break;
+            case '5':
+                std::cout << "Goodbye! It was a pleasure assisting you. "
+                          << "Have a wonderful day!" << std::endl;
+                break;
+            default:
+                std::cout << "Invalid choice. "
+                          << "Please select a number from 1-5."
+                          << std::endl;
             }
         }
         catch (const std::exception& e) {
@@ -186,22 +238,23 @@ int main() {
                       << std::endl;
             std::cin.clear();
         }
-    } while (choice != 5);
+    } while (choice != '5');
 
     return 0;
 }
 
 void display_main_menu() {
-    std::cout << "Main Menu\n"
-              << "1. Ask Fy anything\n"
-              << "2. Credit Questions\n"
-              << "3. Government Aid Questions\n"
-              << "4. College Questions\n"
+    std::cout << "-----Main Menu-----\n"
+              //<< "1. Ask Fy anything\n"
+              << "1. Credit Questions\n"
+              << "2. Government Aid Questions\n"
+              << "3. College Questions\n"
+              << "4. Feminine Questions\n"
               << "5. Exit" << std::endl;
 }
 
 void display_credit_submenu() {
-    std::cout << "Credit Submenu\n"
+    std::cout << "-----Credit Submenu-----\n"
               << "1. What is Credit?\n"
               << "2. Best Starter Credit Cards\n"
               << "3. What are credit rewards?\n"
@@ -212,7 +265,7 @@ void display_credit_submenu() {
 }
 
 void display_government_submenu() {
-    std::cout << "Government Aid Submenu\n"
+    std::cout << "-----Government Aid Submenu-----\n"
               << "1. What California government aid is out there?\n"
               << "2. What is SNAP/TANF?\n"
               << "3. Am I eligible for SNAP/TANF?\n"
@@ -220,10 +273,21 @@ void display_government_submenu() {
 }
 
 void display_college_submenu() {
-    std::cout << "College Submenu\n"
+    std::cout << "-----College Submenu-----\n"
               << "1. What are scholarships?\n"
               << "2. Where can I attend college for free?\n"
               << "3. What is FAFSA?\n"
-              << "4. How do I know I am eligible for FAFSA\n"
+              << "4. How do I know I am eligible for FAFSA?\n"
               << "5. Go back to main menu" << std::endl;
+}
+
+void display_feminine_submenu() {
+    std::cout << "-----Feminine Submenu-----\n"
+              << "1. What is the average pay difference between men and women?\n"
+              << "2. What can women do to fight financial insecurities?\n"
+              << "3. What financial options are there for single mothers?\n"
+              << "4. What is child support?\n"
+              << "5. How can I budget around a child?\n"
+              << "6. How can I budget around multiple children?\n"
+              << "7. Go back to main menu" << std::endl;
 }
